d36006e6d6b43cd5da2505b3f733340378eae3a4
VladKocherga <vlad.kocherga228@gmail.com>
2022-03-26 01:11:39 +0300

    feat: create project structure

---
+++ youtube-client/src/app/components/search/search-item/search-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-item',
+  templateUrl: './search-item.component.html',
+  styleUrls: ['./search-item.component.scss'],
+})
+export default class SearchItemComponent {}
